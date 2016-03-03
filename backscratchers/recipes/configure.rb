f9287b8fef4e5b4c981b91c793791c2a8b946f5c
Leo Critchley <leo@leoc.me>
2016-03-03 18:14:52 +0000

    Initial config, secrets.yml and s3_credentials.yml

---
+++ backscratchers/recipes/configure.rb
@@ -0,0 +1,47 @@
+# AWS OpsWorks recipe to be excuted when configuring the Backscratchers application.
+#
+# This should be run as an additional recipe on a standard Rails App Server layer.
+#
+node[:deploy].each do |application, deploy|
+
+  unless deploy.has_key
+    Chef::Log.debug "Skipping backscratchers::configure for #{application} as it is not the Backscratchers app"
+    next
+  end
+  Chef::Log.info "Configuring Backscratchers application '#{application}'"
+
+  # Add the main user to the web server group.
+  #
+  group 'www-data' do
+    action :modify
+    members 'ubuntu'
+    append true
+  end
+
+  # Create various secret files.
+  #
+  [:secrets,
+   :s3_credentials
+  ].each do |key|
+  template "#{deploy[:deploy_to]}/shared/#{key.to_s}.yml" do
+    source "#{key.to_s}.yml"
+
+    mode 0660
+    group deploy[:group]
+    if platform?('ubuntu')
+      owner 'www-data'
+    elsif platform?('amazon')
+      owner 'apache'
+    end
+
+    variables {
+      "#{key}": deploy.fetch(key),
+      environment: deploy[:rails_env]
+    }
+
+    only_if do
+      deploy[key].present? && File.directory?("#{deploy[:deploy_to]}/shared/")
+    end
+  end
+
+end
