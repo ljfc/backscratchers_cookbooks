7b8176c439a875c5963359e39171115a095b448d
Leo Critchley <leo@leoc.me>
2016-03-07 18:26:22 +0000

    Turn a bunch of stuff off in favour of an in-app callback

--- backscratchers/recipes/deploy.rb
+++ backscratchers/recipes/deploy.rb
@@ -21,33 +21,21 @@ node[:deploy].each do |application, deploy|
   # Adjust permissions on directories that need to be writeable by the webapp/server.
   # TODO @leo Should this go somewhere else? Maybe not. Seems like it’s only needed per-deploy.
   #
-  directory "#{deploy[:deploy_to]}/shared/log" do
-    mode 0770
-  end
-  directory "#{deploy[:deploy_to]}/current/tmp" do
-    mode 0770
-  end
-  #deploy deploy[:deploy_to] do
-  #  begin
-  #    Chef::Log.info "Deployment release path is #{release_path}"
-  #  rescue => e
-  #    Chef::Log.info "There was an error when trying to print the release path"
-  #  end
-  #  before_symlink do
-  #    directory "#{release_path}/tmp" do
-  #      mode 0770
-  #    end
-  #  end
+  #directory "#{deploy[:deploy_to]}/shared/log" do
+  #  mode 0770
+  #end
+  #directory "#{deploy[:deploy_to]}/current/tmp" do
+  #  mode 0770
   #end
 
   # Create various secret files.
   # TODO @leo Figure out how to include the other Xero config files.
   #
-  Chef::Log.info "Processing Backscratchers templates"
-  Chef::Log.info "deploy[:secrets].present? #{deploy[:secrets].present?}"
-  Chef::Log.info "File.directory?(\"\#{deploy[:deploy_to]}/shared/\") #{File.directory?("#{deploy[:deploy_to]}/shared/")}"
-  Chef::Log.info "platform?('ubuntu') #{platform?('ubuntu')}"
-  Chef::Log.info "variables #{{:vars => deploy.fetch(:secrets), :environment => deploy[:rails_env]}.inspect}"
+  #Chef::Log.info "Processing Backscratchers templates"
+  #Chef::Log.info "deploy[:secrets].present? #{deploy[:secrets].present?}"
+  #Chef::Log.info "File.directory?(\"\#{deploy[:deploy_to]}/shared/\") #{File.directory?("#{deploy[:deploy_to]}/shared/")}"
+  #Chef::Log.info "platform?('ubuntu') #{platform?('ubuntu')}"
+  #Chef::Log.info "variables #{{:vars => deploy.fetch(:secrets), :environment => deploy[:rails_env]}.inspect}"
 
   #template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
   #  Chef::Log.info "Processing template secrets.yml"
@@ -63,24 +51,24 @@ node[:deploy].each do |application, deploy|
   #  end
   #end
 
-  [:s3_credentials,
-   :insightly,
-   :xero,
-   :secrets
-  ].each do |key|
-    template "#{deploy[:deploy_to]}/shared/config/#{key.to_s}.yml" do
-      Chef::Log.info "Processing template #{key.to_s}.yml"
-      source "#{key.to_s}.yml.erb"
-      mode 0660
-      user deploy[:user]
-      group deploy[:group]
+  #[:s3_credentials,
+  # :insightly,
+  # :xero,
+  # :secrets
+  #].each do |key|
+  #  template "#{deploy[:deploy_to]}/shared/config/#{key.to_s}.yml" do
+  #    Chef::Log.info "Processing template #{key.to_s}.yml"
+  #    source "#{key.to_s}.yml.erb"
+  #    mode 0660
+  #    user deploy[:user]
+  #    group deploy[:group]
 
-      variables(:vars => deploy.fetch(key), :environment => deploy[:rails_env])
+  #    variables(:vars => deploy.fetch(key), :environment => deploy[:rails_env])
 
-      only_if do
-        deploy[key].present? && File.directory?("#{deploy[:deploy_to]}/shared/")
-      end
-    end
-  end
+  #    only_if do
+  #      deploy[key].present? && File.directory?("#{deploy[:deploy_to]}/shared/")
+  #    end
+  #  end
+  #end
 
 end
