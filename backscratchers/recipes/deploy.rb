7634cbabfce0573eb21ee1a37548dfc08a42ed32
Leo Critchley <leo@leoc.me>
2016-03-07 16:55:49 +0000

    Move per-deploy actions to the deploy recipe

--- backscratchers/recipes/deploy.rb
+++ backscratchers/recipes/deploy.rb
@@ -18,6 +18,15 @@ node[:deploy].each do |application, deploy|
   end
   Chef::Log.info "Deploying Backscratchers application '#{application}'"
 
+  # Adjust permissions on directories that need to be writeable by the webapp/server.
+  # TODO @leo Should this go somewhere else? Maybe not. Seems like it’s only needed per-deploy.
+  #
+  directory "#{deploy[:deploy_to]}/shared/log" do
+    mode 0770
+  end
+  directory "#{deploy[:deploy_to]}/current/tmp" do
+    mode 0770
+  end
   #deploy deploy[:deploy_to] do
   #  begin
   #    Chef::Log.info "Deployment release path is #{release_path}"
@@ -30,5 +39,48 @@ node[:deploy].each do |application, deploy|
   #    end
   #  end
   #end
+
+  # Create various secret files.
+  # TODO @leo Figure out how to include the other Xero config files.
+  #
+  Chef::Log.info "Processing Backscratchers templates"
+  Chef::Log.info "deploy[:secrets].present? #{deploy[:secrets].present?}"
+  Chef::Log.info "File.directory?(\"\#{deploy[:deploy_to]}/shared/\") #{File.directory?("#{deploy[:deploy_to]}/shared/")}"
+  Chef::Log.info "platform?('ubuntu') #{platform?('ubuntu')}"
+  Chef::Log.info "variables #{{:vars => deploy.fetch(:secrets), :environment => deploy[:rails_env]}.inspect}"
+
+  #template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+  #  Chef::Log.info "Processing template secrets.yml"
+  #  source "secrets.yml.erb"
+  #  mode 0660
+  #  user deploy[:user]
+  #  group deploy[:group]
+
+  #  variables(:vars => deploy.fetch(:secrets), :environment => deploy[:rails_env])
+
+  #  only_if do
+  #    deploy[:secrets].present? && File.directory?("#{deploy[:deploy_to]}/shared/")
+  #  end
+  #end
+
+  [:s3_credentials,
+   :insightly,
+   :xero,
+   :secrets
+  ].each do |key|
+    template "#{deploy[:deploy_to]}/shared/config/#{key.to_s}.yml" do
+      Chef::Log.info "Processing template #{key.to_s}.yml"
+      source "#{key.to_s}.yml.erb"
+      mode 0660
+      user deploy[:user]
+      group deploy[:group]
+
+      variables(:vars => deploy.fetch(key), :environment => deploy[:rails_env])
+
+      only_if do
+        deploy[key].present? && File.directory?("#{deploy[:deploy_to]}/shared/")
+      end
+    end
+  end
 
 end
