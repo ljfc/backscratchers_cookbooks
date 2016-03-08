8e65eaaa17667c5e11dd170bdb4e991b211b4277
Leo Critchley <leo@leoc.me>
2016-03-08 16:47:08 +0000

    Change the demo app to something else

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,8 @@
 name 'bs'
 
 depends 'application', '~> 5.1.0'
+depends 'application_git', '~> 1.1.0'
+#depends 'application_javascript', '~> 1.0'
+#epends 'application_python', '~> 4.0'
+depends 'application_ruby', '~> 4.0.1'
+depends 'build-essential', '~> 2.3.1'
--- recipes/demo.rb
+++ recipes/demo.rb
@@ -1,5 +1,35 @@
 Chef::Log.info("*** Installing demo application ***")
 
-application 'Install NetHack' do
-  package 'nethack.x86_64'
+include_recipe 'build-essential'
+package node.value_for_platform(
+  %w{debian ubuntu} => {default: %w{libmysqlclient-dev libpq-dev libsqlite3-dev}},
+  %w{redhat centos} => {'~> 7.0' => %w{mariadb-devel postgresql-libs postgresql-devel sqlite-devel},
+                        '~> 6.0' => %w{mysql-devel postgresql-libs postgresql-devel sqlite-devel}},
+)
+
+# Application resource for Todo_Express.
+application node['todo_rails']['path'] do
+  # Clone the source code from GitHub.
+  git 'https://github.com/engineyard/todo.git'
+  # Install Ruby.
+  ruby_runtime 'any' do
+    version ''
+  end
+  # Install node for execjs and link it globally.
+  nodejs = javascript 'nodejs'
+  link '/usr/bin/node' do
+    to nodejs.javascript_binary
+  end
+  # Run `bundle install` to install dependencies.
+  bundle_install
+  # Handle Rails deployment.
+  rails do
+    database 'sqlite3:///db.sqlite3'
+    secret_token 'd78fe08df56c9'
+    migrate true
+  end
+  # Create a system service to run Unicorn on port 8000.
+  unicorn do
+    port 8000
+  end
 end
