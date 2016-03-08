aba329719242199fe4f4a1139d5fe745b48a3310
Leo Critchley <leo@leoc.me>
2016-03-08 18:24:37 +0000

    Draft bs::deploy
    Based on simple example here: https://github.com/poise/application_ruby/blob/master/test/cookbooks/application_ruby_test/recipes/rails.rb

--- Berksfile
+++ Berksfile
@@ -2,7 +2,6 @@ source "https://supermarket.chef.io"
 
 metadata
 
-
 cookbook 'application', '~> 5.1.0'
 cookbook 'application_git', '~> 1.1.0'
 cookbook 'application_javascript', '~> 1.0.0'
---
+++ recipes/deploy.rb
@@ -0,0 +1,32 @@
+app = search('aws_opsworks_app', 'shortname:backscratchers').first
+db = search('aws_opsworks_rds_db_instance').first
+
+application '/backscratchers' do
+
+  ruby '2.1.3'
+
+  git do
+    repository app['app_source']['url']
+    deploy_key app['app_source']['ssh_key']
+  end
+
+  bundle_install do
+    deployment true
+    without %w{development test}
+  end
+
+  rails do
+    database do
+      adapter 'mysql2'
+      host db['address']
+      username db['db_user']
+      password db['db_password']
+      database db['db_instance_identifier']
+    end
+  end
+
+  unicorn do
+    port 8000
+  end
+
+end
