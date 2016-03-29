ea0d3de91b761d644ac10a2382cffb1cd6f36b56
Leo Critchley <leo@leoc.me>
2016-03-29 14:03:57 +0100

    Install NGINX with Passenger

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,3 +1,11 @@
 Chef::Log.info("The Backscratchers deploy recipe called")
 
-
+# Restart or start nginx.
+Chef::Log.info("NGINX deployment")
+service 'nginx' do
+  if File.exists? '/opt/nginx/logs/nginx.pid' # TODO @leo Check what the correct path is for this on Ubuntu 14.04
+    action :restart
+  else
+    action :start
+  end
+end
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,11 +5,24 @@ include_recipe 'apt::default'
 
 # Install Passenger, see
 # https://www.phusionpassenger.com/library/install/nginx/install/oss/trusty/
-Chef::Log.inf("Installing Passenger")
+Chef::Log.info("Passenger setup")
+# Install dependencies.
 package 'apt-transport-https'
 package 'ca-certificates'
+# Add the Passenger APT repository.
+apt_repository 'passenger' do
+  uri 'https://oss-binaries.phusionpassenger.com/apt/passenger'
+  distribution 'trusty'
+  components ['main']
 
-Chef::Log.info("Installing NGINX")
+  keyserver 'keyserver.ubuntu.com'
+  key '561F9B9CAC40B2F7'
+end
+# Install Passenger itself, plus nginx extras.
+package 'nginx-extras'
+package 'passenger'
+
+Chef::Log.info("NGINX setup")
 package 'nginx'
 service 'nginx' do
   action [:enable, :start]
