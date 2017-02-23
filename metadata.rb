e5312e5c51b1b0585c6760640132ca7db28e7bbc
Leo Critchley <leo@leoc.me>
2017-02-23 16:01:33 +0000

    Make the report recipe optional based on { "report": "true" } being set

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'The Backscratchers'
 maintainer_email 'leo@thebackscratchers.com'
 description 'Manages The Backscratchers infrastructure'
 long_description 'Manages infrastructure for The Backscratchers website on Amazon Web Services OpsWorks'
-version '0.0.0'
+version '1.1.0'
 
 depends 'apt', '~> 3.0.0' # So we can update apt and install the latest packages.
 
--- recipes/report.rb
+++ recipes/report.rb
@@ -1,45 +1,51 @@
-Chef::Log.info("*** Backscratchers Report ***")
+if node.has_key?('report') && node['report'] == true
 
-def bs_report(data)
-  if data.respond_to?(:'any?') && data.any?
-    Chef::Log.info("\t\t[")
-    data.each do |item|
-      if item.respond_to?(:'any?') && item.any?
-        Chef::Log.info("\t\t\t[")
-        item.each do |sub_item|
-          Chef::Log.info("\t\t\t\t#{sub_item.inspect}")
+  Chef::Log.info("*** Backscratchers Report ***")
+
+  def bs_report(data)
+    if data.respond_to?(:'any?') && data.any?
+      Chef::Log.info("\t\t[")
+      data.each do |item|
+        if item.respond_to?(:'any?') && item.any?
+          Chef::Log.info("\t\t\t[")
+          item.each do |sub_item|
+            Chef::Log.info("\t\t\t\t#{sub_item.inspect}")
+          end
+          Chef::Log.info("\t\t\t]")
+        else
+          Chef::Log.info("\t\t\t#{item.inspect}")
         end
-        Chef::Log.info("\t\t\t]")
-      else
-        Chef::Log.info("\t\t\t#{item.inspect}")
       end
+      Chef::Log.info("\t\t]")
+    else
+      Chef::Log.info("\t\t#{data.inspect}")
     end
-    Chef::Log.info("\t\t]")
-  else
-    Chef::Log.info("\t\t#{data.inspect}")
   end
+
+  Chef::Log.info("Node contents:")
+  node.keys.each do |key|
+    Chef::Log.info("\t#{key}")
+    bs_report node[key]
+  end
+
+  Chef::Log.info("Data bag contents:")
+  data_bag_indices = [:aws_opsworks_app,
+                      :aws_opsworks_command,
+                      :aws_opsworks_ecs_cluster,
+                      :aws_opsworks_elastic_load_balancer,
+                      :aws_opsworks_instance,
+                      :aws_opsworks_layer,
+                      :aws_opsworks_rds_db_instance,
+                      :aws_opsworks_stack,
+                      :aws_opsworks_user]
+
+  data_bag_indices.each do |index|
+    Chef::Log.info("\t#{index}")
+    bs_report search(index.to_s)
+  end
+
+  Chef::Log.info("*** Report complete ***")
+
+else
+  Chef::Log.info("*** Skipping Backscratchers report as it was not requested ***")
 end
-
-Chef::Log.info("Node contents:")
-node.keys.each do |key|
-  Chef::Log.info("\t#{key}")
-  bs_report node[key]
-end
-
-Chef::Log.info("Data bag contents:")
-data_bag_indices = [:aws_opsworks_app,
-                    :aws_opsworks_command,
-                    :aws_opsworks_ecs_cluster,
-                    :aws_opsworks_elastic_load_balancer,
-                    :aws_opsworks_instance,
-                    :aws_opsworks_layer,
-                    :aws_opsworks_rds_db_instance,
-                    :aws_opsworks_stack,
-                    :aws_opsworks_user]
-
-data_bag_indices.each do |index|
-  Chef::Log.info("\t#{index}")
-  bs_report search(index.to_s)
-end
-
-Chef::Log.info("*** Report complete ***")
